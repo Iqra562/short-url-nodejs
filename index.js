4f83f1ddbfaa2fc750d12dbac6f5ab82c474a3bc
Iqra Khaliq <114660239+Iqra562@users.noreply.github.com>
2025-02-18 21:56:33 +0500

    Update index.js

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const PORT = 8001;
 
 connectToMongoDB('mongodb+srv://iqra44938:mtnwR3jdxJ4dUhl1@cluster0.rd6d3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
 .then(()=>console.log('mongodb connected'))
-app.use('url',urlRoute)
+
+app.use(express.json())
+
+app.use("/url",urlRoute)
 
 app.listen(PORT,()=>console.log('Server started')) 
